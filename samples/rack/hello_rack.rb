20fa98c44f81f06d9a048ad037595155b2b3a3d7
Masao Mutoh <mutomasa@gmail.com>
2010-03-13 02:20:06 +0900

    Cleanup the rack sample.

--- samples/rack/hello_rack.rb
+++ samples/rack/hello_rack.rb
@@ -1,14 +1,13 @@
 require 'rubygems'
 require 'rack'
-require 'locale'
+require 'locale_rack'
  
-Locale.init(:driver => :cgi)
-
 class HelloRackApp
+  include Locale::Rack
+
   def call(env)
     req = Rack::Request.new(env)
-    Locale.set_request([req["lang"]], [req.cookies["lang"]],
-                       env["HTTP_ACCEPT_LANGUAGE"], env["HTTP_ACCEPT_CHARSET"])
+    init_locale(env, req)
     str = "Language tag candidates of your request order by the priority:\n\n"
     str += Locale.candidates(:type => :rfc).map{|v| v.inspect + "\n"}.join
     [200, {"Content-Type" => "text/plain", "Content-Length" => str.length.to_s}, [str]]
